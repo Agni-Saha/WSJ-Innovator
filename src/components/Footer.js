f2e2cf3e64becb101a38c76c9c585e077dac10f4
Agni-Saha <agnisaha337599@gmail.com>
2022-01-31 17:22:55 +0530

    created banner with editor letter portion

---
+++ src/components/Footer.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <div className="wrapper">
+                <div className="marquee">
+                    <p className='hover_underline'> Design </p>
+                    <p className='hover_underline'> Education </p>
+                    <p className='hover_underline'> Entertainment </p>
+                    <p className='hover_underline'> Entrepreneurship </p>
+                    <p className='hover_underline'> Fashion </p>
+                    <p className='hover_underline'> Film </p>
+                    <p className='hover_underline'> Food </p>
+                    <p className='hover_underline'> Humanitarian </p>
+                    <p className='hover_underline'> Literature </p>
+                    <p className='hover_underline'> Music </p>
+                    <p className='hover_underline'> Performing Arts </p>
+                    <p className='hover_underline'> Philantrophy </p>
+                    <p className='hover_underline'> Pop Culture </p>
+                    <p className='hover_underline'> Public Service </p>
+                    <p className='hover_underline'> Social Justice </p>
+                    <p className='hover_underline'> Sports </p>
+                    <p className='hover_underline'> Technology </p>
+                    <p className='hover_underline'> Television </p>
+                    <p className='hover_underline'> Acting & Activism </p>
+                    <p className='hover_underline'> Architechture </p>
+                    <p className='hover_underline'> Art </p>
+                    <p className='hover_underline'> Brand </p>
+                    <p className='hover_underline'> Design </p>
+                    <p className='hover_underline'> Education </p>
+                    <p className='hover_underline'> Entertainment </p>
+                    <p className='hover_underline'> Entrepreneurship </p>
+                    <p className='hover_underline'> Fashion </p>
+                    <p className='hover_underline'> Film </p>
+                    <p className='hover_underline'> Food </p>
+                    <p className='hover_underline'> Humanitarian </p>
+                    <p className='hover_underline'> Literature </p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
