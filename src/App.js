fa04d462e54822a1bbe1d7a4651cab04deee1d4c
Agni-Saha <agnisaha337599@gmail.com>
2022-02-03 23:23:44 +0530

    added the loading screen

--- src/App.js
+++ src/App.js
@@ -1,15 +1,55 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import "./App.css"
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
-import "./App.css"
 import Body from './components/Body';
+import Loading from "./components/Loading"
+import gsap from "gsap"
 
 function App() {
+    useEffect(() => {
+        let tl = gsap.timeline({
+            ease: "power2.out"
+        })
+
+        tl
+            .to(".wipe", {
+                y: "-100vh",
+                duration: 0.8,
+                delay: 3
+            })
+            .to(".loading svg", {
+                css: {
+                    fill: "white"
+                },
+                duration: 0.4,
+                delay: -0.6
+            })
+            .to(".wipe", {
+                y: "-200vh",
+                duration: 0.8,
+                delay: 2
+            })
+            .to(".loading", {
+                display: "none",
+                opacity: 0,
+                duration: 0.8
+            })
+            .to(".App_Container", {
+                display: "block",
+                duration: 0.8,
+                delay: -0.8,
+                ease: "expo.out"
+            })
+    }, [])
     return (
         <div className='App'>
-            <Navbar />
-            <Body />
-            <Footer />
+            <Loading />
+            <div className="App_Container">
+                <Navbar />
+                <Body />
+                <Footer />
+            </div>
         </div>
     );
 }
